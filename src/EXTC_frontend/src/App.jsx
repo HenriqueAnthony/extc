ff61402631e6afc0f3e03492e5114d5fe2fb2010
Henrique <henriqueanthonycs@gmail.com>
2025-05-14 18:55:46 -0300

    segunda parte

--- src/EXTC_frontend/src/App.jsx
+++ src/EXTC_frontend/src/App.jsx
@@ -1,31 +1,58 @@
-import { useState } from 'react';
+import React from "react";
+import TokenCard from "./components/TokenCard";
+import { useState, useEffect } from 'react';
 import { EXTC_backend } from 'declarations/EXTC_backend';
+import TransferForm from "./components/TransferForm";  
+
+
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [name, setName] = useState('');
+  const [symbol, setSymbol] = useState('');
+  const [fee, setFee] = useState(0);
+  const [supply, setSupply] = useState(0);
+  const [minter, setMinter] = useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    EXTC_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
+  useEffect(() => {
+    const init = async () => {
+        await getTokenInfo();
+    }
+    init();
+  }, []);
+
+  async function getTokenInfo() {
+   
+    try {
+      let info = await EXTC_backend.getTokenInfo();  
+      if( info != null &&  info != undefined ){
+        setName(info.name);
+        setSymbol(info.symbol);
+        setFee(parseInt(info.fee));
+        setSupply(parseInt(info.totalSupply));
+        setMinter(info.mintingPrincipal);      
+      }
+    } catch (error) {
+      alert("Ocorreu uma falha ao obter informações!");  
+    }
+   
   }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+        <div className="card" >
+            <TokenCard
+                name={name}
+                symbol={symbol}
+                fee={fee}
+                supply={supply}
+                minter={minter}
+            />
+
+            <TransferForm />
+
+        </div>
     </main>
   );
 }
 
-export default App;
+export default App;  
